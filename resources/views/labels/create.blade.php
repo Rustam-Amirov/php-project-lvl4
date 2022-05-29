c85f7add274bc36335ffe63c834213a69e904d99
rustam-amirov <r.amirov@yahoo.com>
2022-05-29 22:36:27 +0300

    added validation messages

--- resources/views/labels/create.blade.php
+++ resources/views/labels/create.blade.php
@@ -9,12 +9,22 @@
             @csrf
             <div class="form-group mb-3">
                 <label for="name">{{__('label.t_name')}}</label>
-                <input class="form-control" name="name" type="text" id="name">
+                <input class="form-control  @error('name') is-invalid @enderror" name="name" type="text" id="name">
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{$message}}</strong>
+                    </span>
+                @enderror
             </div>
 
             <div class="form-group mb-3">
                 <label for="description">{{__('label.t_description')}}</label>
-                <textarea class="form-control" name="description" cols="50" rows="10" id="description"></textarea>
+                <textarea class="form-control  @error('description') is-invalid @enderror" name="description" cols="50" rows="10" id="description"></textarea>
+                @error('description')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{$message}}</strong>
+                    </span>
+                @enderror
             </div>
 
             <input class="btn btn-primary mt-3" type="submit" value="{{__('label.create_button')}}">
